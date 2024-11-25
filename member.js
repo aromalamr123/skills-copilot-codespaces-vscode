5c60efe3378025bf9c406ac50127e3aa64144f6f
Aromal M R <77744567+aromalamr123@users.noreply.github.com>
2024-11-25 21:18:22 +0000

    Copilot Second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  document.getElementById("skills").classList.remove("hide");
+  document.getElementById("members").classList.add("hide");
+  document.getElementById("skillsTab").classList.add("active");
+  document.getElementById("membersTab").classList.remove("active");
+}
